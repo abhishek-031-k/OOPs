be10dfff797ddcb7215756ec96092fccc7d2eb0c
Abhishek <abhishkumar159@gmail.com>
2026-06-30 19:27:45 +0530

    Refactor code structure for improved readability and maintainability

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,16 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Student{
-    public:
-  string name;
-
-  Student(){
-    cout<<"non-parameterized constructor"<<endl;
-  }
+class Print{
+public:
+void show(int x){
+  cout<<"int: "<<x<<endl;
+}
+void show(char ch){
+  cout<<"char: "<<ch<<endl;
+}
 };
 
 int main(){
-
+  Print p1;
+  p1.show(1);
     return 0;
 }
