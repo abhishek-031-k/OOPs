354e6e1478982510594f36a47345cfc48817d30e
Abhishek <abhishkumar159@gmail.com>
2026-08-02 23:47:14 +0530

    Refactor code structure for improved readability and maintainability

---
+++ hybrid.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class base{
+    public:
+    int parent;
+    base(){
+        cout<<"base class constructor"<<endl;
+    }
+};
+
+class derivedclass1 : public base{
+    public: 
+    derivedclass1(){
+        cout<<"derived_class1 constructor"<<endl;
+    }
+};
+
+class derivedclass2 : public base{
+    public:
+    derivedclass2(){
+        cout<<"derived_class2 constructor"<<endl;
+    }
+};
+
+class multipleclass : public derivedclass1, public derivedclass2{
+    public:
+    multipleclass(){
+        cout<<"multipleclass constructor"<<endl;
+    }
+};
+
+int main(){
+    multipleclass m1;
+    return 0;
+}
